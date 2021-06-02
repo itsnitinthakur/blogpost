bc39712850ad0739d871d549fcd2f82d685eaa8b
Nitin <itsnitinthakur@gmail.com>
2021-06-02 10:28:55 +0530

    4th attempt heroku(adding staticfiles_dirs)

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'iCoder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
